0ef119b1b6522b69f5fb0d92d656561ecd50d434
Руслан <rus94.5610@gmail.com>
2023-01-31 22:07:47 +0300

    Home work 4

---
+++ hw4/task29/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rand = new Random();
+
+int ArrayFilling() {
+
+int[] array = new int[8]; 
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0, 50);
+}
+System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+return 0;
+}
+
+Console.Clear();
+
+ArrayFilling();
