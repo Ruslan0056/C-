42ef20a597961c6d6043ac48e021e6bb3de9095a
Руслан <rus94.5610@gmail.com>
2023-02-01 21:15:43 +0300

    Home work 5

---
+++ hw5/task38/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+
+double[] FillArr(int size, int leftRange, int rightRange)
+{
+    double[] arr = new double[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(rand.Next(leftRange, rightRange + 1) + rand.NextDouble(), 3);
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+void FindMaxAndMin(double[] arr, out double max, out double min)
+{
+    max = 0;
+    min = 0;
+    for (int i = 0; i < arr.Length; i++) {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+        if (min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+}
+
+
+double[] array = FillArr(5, -5, 5);
+
+PrintArray(array);
+FindMaxAndMin(array, out double max, out double min);
+System.Console.WriteLine($"Максимальное число массива {max}, минимальное число массива {min}.");
+
+
