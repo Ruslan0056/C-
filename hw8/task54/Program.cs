6fa0219d3978a1e09a5a47fe34e172d55fca2fbb
Руслан <rus94.5610@gmail.com>
2023-02-14 00:15:52 +0300

    Home work 8

---
+++ hw8/task54/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.Clear();
+
+Random rand = new Random();
+int[,] Fillmatrix(int rows, int columns)
+{
+    int[,] mtrx = new int[rows,columns];
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                mtrx[i,j] = rand.Next(-10, 11);
+            }
+        }
+    } 
+    return mtrx;
+}
+
+void PrintMatrix(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+        {
+            for (int j = 0; j < mtrx.GetLength(1); j++)
+            {
+                System.Console.Write(mtrx[i,j] + "\t");
+            }
+            System.Console.WriteLine();
+    }    
+}
+
+int[,] Streamline(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < mtrx.GetLength(1) - 1; k++)
+                if (mtrx[i,k] < mtrx[i,k+1])
+                {
+                    int max = mtrx[i,k];
+                    mtrx[i,k] = mtrx[i,k+1];
+                    mtrx[i,k+1] = max;
+                }
+
+        }
+    }
+    return mtrx;
+}
+
+System.Console.WriteLine("Введите размеры матрицы!");
+System.Console.Write("Количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = Fillmatrix(n,m);
+PrintMatrix(matrix);
+
+System.Console.WriteLine();
+matrix = Streamline(matrix);
+PrintMatrix(matrix);
+
+
+
+
+
+
---
+++ hw8/task62/Program.cs
@@ -0,0 +1,96 @@
+﻿Console.Clear();
+
+void PrintMatrix(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+        {
+            for (int j = 0; j < mtrx.GetLength(1); j++)
+            {
+                System.Console.Write(mtrx[i,j] + "\t");
+            }
+            System.Console.WriteLine();
+    }    
+}
+
+
+int[,] FillSpiral(int rows, int cols, int startNumber)
+{
+    int[,] spiralArray = new int[rows, cols];
+    int count = startNumber;
+    int startCol = 0;
+    int endCol = cols - 1;
+    int startRow = 0;
+    int endRow = rows - 1;
+        
+
+    while (count - startNumber < rows * cols)    
+    {
+        for (int i = startCol; i <= endCol; i++)
+        {
+            spiralArray[startRow,i] = count;
+            count++;
+        }
+        startRow++;
+
+        if (count - startNumber >= rows * cols) {
+            break;
+        }
+
+        for (int i = startRow; i <= endRow; i++)
+        {
+            spiralArray[i,endCol] = count;
+            count++;
+        }
+        endCol--;
+
+        if (count - startNumber >= rows * cols) {
+            break;
+        }
+
+        for (int i = endCol; i >= startCol; i--)
+        {
+            spiralArray[endRow,i] = count;
+            count++;
+        }
+        endRow--;
+
+        if (count - startNumber >= rows * cols) {
+            break;
+        }
+
+        for (int i = endRow; i >= startRow; i--)
+        {
+            spiralArray[i,startCol] = count;
+            count++;
+        }
+        startCol++;
+
+    }
+    return spiralArray;
+}
+
+
+
+
+
+System.Console.WriteLine("Введите размеры первой матрицы!");
+System.Console.Write("Количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите стартовое число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = FillSpiral(n,m, number);
+PrintMatrix(matrix);
+
+
+
+
+
+
+
+
+
+
