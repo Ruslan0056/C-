319153db1b77a86eb6ceb4cd0c75c63ef173749f
Руслан <rus94.5610@gmail.com>
2023-02-15 00:07:29 +0300

    Home work 9

---
+++ hw9/task66/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int SumElem(int m, int n)
+{
+    if (n >= m)
+    {
+        return n + SumElem(m, n - 1);
+    }
+    return 0;
+}
+
+
+System.Console.WriteLine(SumElem(1, 15));
+
+System.Console.WriteLine(SumElem(4, 8));
